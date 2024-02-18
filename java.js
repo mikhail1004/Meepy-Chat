c0291acdec558888539e6d85d66cd55bba627568
Mikhail1004 <126953888+mikhail1004@users.noreply.github.com>
2024-02-18 11:59:26 +0000

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCAXB5cOlNR4bPSSsEJ2i6hAnTK17UzdNI",
+    authDomain: "test-3e03b.firebaseapp.com",
+    databaseURL: "https://test-3e03b-default-rtdb.firebaseio.com",
+    projectId: "test-3e03b",
+    storageBucket: "test-3e03b.appspot.com",
+    messagingSenderId: "651291747053",
+    appId: "1:651291747053:web:b629a90b387f2f75b362e4"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
