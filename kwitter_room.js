62d16b4ec2d6942474cb23d916c6bbdee69cb81f
Mikhail1004 <126953888+mikhail1004@users.noreply.github.com>
2024-03-06 18:40:27 +0000

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -11,7 +11,6 @@ const firebaseConfig = {
   // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
 //ADD YOUR FIREBASE LINKS HERE
-
 user_name = localStorage.getItem("user_name");
 document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
 
@@ -23,8 +22,7 @@ function addRoom()
                 purpose : "adding room name"
           });
 
-          localStorage.setItem("room_name", room);
-
+          localStorage.setItem("room_name", name);
           window.location = "kwitter_page.html";
     }
 
@@ -41,14 +39,39 @@ getData();
 
 function redirectToRoomName(name)
 {
-    console.log(name);
+    console.log(room_name);
     localStorage.setItem("room_name", name);
-    window.location = "kwitter_page.html";
+    window.location = "kwitter.html";
 }
 
 function logout()
 {
     localStarage.removeItem("user_name");
     localStorage.removeItem("room_name");
-          window.location = "kwitter.html";
+          window.location = "index.html";
 }
+
+function logout()
+{
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location.replace("index.html")
+}
+
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+    firebase_message_id = childKey;
+    message_data = childData;
+console.log(firebase_message_id);
+console.log(message_data);
+name = message_data['name'];
+message = message_data['message'];
+like = message_data['like'];
+name_with_tag = "<h4>" + name + "<img class='user_tick' src='tick.png'></h4>";
+message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
+like_button = "<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
+span_with_tag = "<span class='glyphcon glyphcon-thumbs-up'>Like: " + like + "</span></button><hr>";
+
+row = name_with_tag + message_with_tag + like_button + span_with_tag;
+document.getElementById("output").innerHTML += row;
+} });  }); }
+getData();
